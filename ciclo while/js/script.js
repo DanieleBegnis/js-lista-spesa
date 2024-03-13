ec2cf5f8b40a945c8121b2c950e5b9c3b2eb5623
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-13 17:06:33 +0100

    feat add while cicle

--- ciclo while/js/script.js
+++ ciclo while/js/script.js
@@ -8,16 +8,20 @@ const list = [
     'Piatti pronti'
 ];
 
-
+const shoppingList = document.querySelector('.lista-spesa');
 //stampo sulla pagina indvidualmente gli elementi della lista, usando il ciclo while
 
 let i = 0;
 while(i < list.length) {
-    console.log(list[i]);
-
+    const thisPath = list[i];
+    const listItem = `
+    <div>
+        ${thisPath}
+    </div>
+    `;
+    shoppingList.innerHTML += listItem
     i++;
 }
-
 
 
 
