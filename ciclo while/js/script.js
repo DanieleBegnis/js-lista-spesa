8eb79dd0b1e18da68aa52be3cf45779acbac5a78
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-13 16:30:12 +0100

    feat: add while and console log

---
+++ ciclo while/js/script.js
@@ -0,0 +1,20 @@
+//prendo lista della spesa
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+const shoppingList = document.querySelector('.lista-spesa');
+//stampo sulla pagina indvidualmente gli elementi della lista, usando il ciclo for
+let i = 0;
+while(i < list.lenght) {
+    console.log(list[i])
+    i++;
+}
+
+
+
+
