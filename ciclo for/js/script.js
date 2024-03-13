6963e0c14ecd8cc023bdb9172359a8b4cfe071fc
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-13 16:15:59 +0100

    add comments and for

---
+++ ciclo for/js/script.js
@@ -0,0 +1,13 @@
+//prendo lista della spesa
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+//stampo sulla pagina indvidualmente gli elementi della lista, usando il ciclo for
+for(let i = 0; i < list.length; i++) {
+    
+}
